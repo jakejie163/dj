eb4992229bf01239533300502b84b44ad4d35f8c
ch1huizong <ch1huizong@gmail.com>
2017-08-14 00:08:20 +0800

    侧边栏增加了日期归档, 基础模板微调了颜色和对翻页控件进行了修改

--- blogsite/mysite/blog/templatetags/blog_tags.py
+++ blogsite/mysite/blog/templatetags/blog_tags.py
@@ -11,16 +11,23 @@ from ..models import Post
 def total_posts():
     return Post.published.count()
 
-@register.inclusion_tag('blog/post/latest_posts.html')
+@register.inclusion_tag('blog/post/show_months.html')
+def show_months_of_date():
+    months_of_date = Post.published.datetimes('publish', 'month', 'DESC')
+    return {'months_of_date': months_of_date}
+
+
+@register.inclusion_tag('blog/post/side_posts.html')
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
-    return {'latest_posts': latest_posts}
+    return {'side_posts': latest_posts}
 
-@register.assignment_tag
+@register.inclusion_tag('blog/post/side_posts.html')
 def get_most_commented_posts(count=5):
-    return Post.published.annotate(
+    most_commented_posts = Post.published.annotate(
                 total_comments=Count('comments')
                 ).order_by('-total_comments')[:count]
+    return {'side_posts': most_commented_posts}
 
 @register.filter(name="markdown")
 def markdown_format(text):

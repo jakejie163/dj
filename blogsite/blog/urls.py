0169ad3ac0f4013ca7b11bb4338b88d81d3ae690
ch1huizong <ch1huizong@gmail.com>
2017-12-17 23:14:35 +0800

    blogsite增加feed

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -14,5 +15,7 @@ urlpatterns = [
 
     path('<int:post_id>/share/', 
         views.post_share,
-        name='post_share')
+        name='post_share'),
+
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]

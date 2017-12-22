e118a90c6350eb8b79c2a563cbb74ca405f63f70
ch1huizong <ch1huizong@gmail.com>
2017-12-22 11:10:42 +0800

    新增onlineshop项目，基于django2.0

--- onlineshop/onlineshop/urls.py
+++ onlineshop/onlineshop/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""onlineshop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,16 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('news/', include('news.urls')),
-    path('blog/', include('blog.urls')),
-
-    path('author-polls/', include('polls.urls', namespace='author-polls')),
-    path('publisher-polls/', include('polls.urls', namespace='publisher-polls')),
-
-    path('books/', include('books.urls')),
 ]

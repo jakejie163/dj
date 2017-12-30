d6a3743c4a4861339fd452da53f076700362ce57
ch1huizong <ch1huizong@gmail.com>
2017-12-30 23:52:12 +0800

    onlineshop的cart购物车应用完成

--- onlineshop/shop/views.py
+++ onlineshop/shop/views.py
@@ -3,7 +3,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
-#from cart.forms import CartAddProductForm
+from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -26,12 +26,12 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
-    #cart_product_form = CartAddProductForm()
+    cart_product_form = CartAddProductForm()
     return render(
                     request,
                     'shop/product/detail.html',
                     {
                         'product': product,
+                        'cart_product_form': cart_product_form,
                     }
                 )
-                    #'cart_product_form': cart_product_form})

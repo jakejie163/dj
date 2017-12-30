51e773c577e3f8944bf91f9fdd296944cce15b10
ch1huizong <ch1huizong@gmail.com>
2017-12-30 21:42:01 +0800

    onlineshop的cart应用增加Cart管理类

--- onlineshop/onlineshop/settings.py
+++ onlineshop/onlineshop/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     'django_extensions',
 
     'shop',
+    'cart',
 ]
+
+CART_SESSION_ID = 'cart'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -124,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
